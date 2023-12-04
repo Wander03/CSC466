17f4ca5e5cfc1d93972bff6856a974148e5c0019
brendad8 <72055001+brendad8@users.noreply.github.com>
2023-12-03 17:59:34 -0800

    knnAuthorship using files from out directory

--- lab6/knnAuthorship.py
+++ lab6/knnAuthorship.py
@@ -13,46 +13,92 @@ Description:
                     <K: number of neighbors to consider> 
                     <distance metric: 0 - cosine, 1 - okapi>
 """
+import sys
+import time
 import pandas as pd
 import numpy as np
+import random
 from sys import argv
 from Vector import Vector
 
 
-def knn_classifier(D, C, k, dist):
-    # REWORK FOr USE OF VECTORS AND MAKE SURE TO IGNORE ITSELF (REMOVE EXACTY THE SAME =1???)
-    if dist != 3:
-        closest_neighbors = distances.T.apply(lambda row: row.nsmallest(k).index.tolist(), axis=1)
-    else:
-        closest_neighbors = distances.T.apply(lambda row: row.nlargest(k).index.tolist(), axis=1)
-    
+def cosine_sim(d1, d2):
+    return d2.apply(lambda row: np.dot(d1.to_numpy(), row.to_numpy()) / (np.linalg.norm(d1.to_numpy()) * np.linalg.norm(row.to_numpy())), axis=1)
+
+
+def knn_classifier(ground_truth, k, distances):
+    closest_neighbors = distances.T.apply(lambda row: row.nlargest(k+1).index.tolist()[1:], axis=1)
+
     predictions = {}
     for i, row in enumerate(closest_neighbors):
-        predictions[D.iloc[i]["index"]] = D.iloc[row][C].value_counts().index.tolist()[0]
+        predictions[i] = ground_truth.iloc[row]['author'].value_counts().index.tolist()[0]
 
     return predictions
 
-def main(argv):
-    ground_truth = pd.read_csv('./vectorized_data/ground_truth.csv')
-    df = pd.read_csv('./vectorized_data/doc_freqs.csv', na_filter=False)
-    tf = pd.read_csv('./vectorized_data/term_freqs.csv')
-    tf_idf = pd.read_csv('./vectorized_data/tf_idf.csv')
-    f = Vector(ground_truth['file'][1], ground_truth['author'][1], tf.iloc[1], tf_idf.iloc[1], ground_truth['size'][1])
-    g = Vector(ground_truth['file'][0], ground_truth['author'][0], tf.iloc[0], tf_idf.iloc[0], ground_truth['size'][0]) 
-    avg = ground_truth['size'].mean()
-    print(g.okapi_similarity(f, df, avg))
-    breakpoint()
-    # fix okapi? ordering of docs consistent?
+def main():
 
-    # # UPDATE THIS
-    # D = pd.read_csv(argv[1], skiprows=[1, 2], dtype=str)
-    # K = int(argv[3])
-    # dist = int(argv[4])
+    start = time.time()
+    args = sys.argv
 
-    # predictions = knn_classifier_same(D_clean, A, C, K, dist) if flag else knn_classifier_different(D_clean, A, C, classify.copy(), K, dist)
-    # D["pred_class"] = D.index.map(predictions)
-    
-    # D.to_csv(f".\\results_KNN\\{name[:-4]}-results.out.csv", index=False)
+    gt = args[1]
+    ground_truth = pd.read_csv(gt)
+
+    tfidf = args[2]
+    tf_idf = pd.read_csv(tfidf)
+
+    sim = int(args[3])
+
+    if sim == 0:
+        okapi=False
+    elif sim == 1:
+        okapi=True
+    else:
+        print('Please use valid similarity metric:')
+        print('0: Cosine Similarity')
+        print('1: Okapi')
+        return None
+
+    if okapi:
+        k = int(args[4])
+        df = args[5]
+        tf = args[6]
+        docf = pd.read_csv(df, na_filter=False)
+        termf = pd.read_csv(tf)
+        N = int(args[7])
+
+        sample = random.sample(range(5000), N)
+
+        vectors = []
+        for i  in range(ground_truth.shape[0]):
+            vectors.append(Vector(ground_truth['file'][i], ground_truth['author'][i], termf.iloc[i], tf_idf.iloc[i],
+                   ground_truth['size'][i]))
+
+        avg = ground_truth['size'].mean()
+        distances = pd.DataFrame()
+        for i in sample:
+            v1 = vectors[i]
+            sims = []
+            for j in range(ground_truth.shape[0]):
+                sims.append(v1.okapi_similarity(vectors[j], docf, avg))
+            distances = pd.concat([distances, pd.Series(sims, name=i)], axis=1)
+
+        predictions = knn_classifier(ground_truth, k, distances)
+        temp = ground_truth.iloc[sample][['author', 'file']].reset_index(drop=True)
+        preds = pd.Series(list(predictions.values()))
+        temp['prediction'] = preds
+        temp[['file', 'prediction']].to_csv('out/predictions.csv', index=False)
+
+
+    else:
+        k = int(args[4])
+        distances = pd.read_csv('out/dist_matrix.csv')
+        # distances = tf_idf.apply(cosine_sim, args=(tf_idf,), axis=1)
+        predictions = knn_classifier(ground_truth, k, distances)
+        temp = ground_truth[['author', 'file']]
+        preds = pd.Series(list(predictions.values()))
+        temp['prediction'] = preds
+        temp[['file', 'prediction']].to_csv('out/predictions.csv', index=False)
+
 
 if __name__ == "__main__":
-    main(argv)
+    main()
